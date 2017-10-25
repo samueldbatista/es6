041c0d21ea1b1087370ecc0e759370805cf85b00
Samuel <samueldantas7@hotmail.com>
2017-10-25 12:34:03 -0300

    Index.js lintado.

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ class Message {
  * @extends {Message}
  */
 class ImageMessage extends Message {
-  constructor(text = '', created = Date.now(),
-              url = '', thumbnail = '') {
+  constructor(
+    text = '', created = Date.now(),
+    url = '', thumbnail = '',
+  ) {
     super(text, created);
     this.url = url;
     this.thumbnail = thumbnail;
@@ -52,9 +54,9 @@ class ImageMessage extends Message {
 }
 
 // Message instances
-var emptyMessage = Message.newEmptyMessage();
-var textMessage = new Message('Yesterday message', Date.now() - 86400);
-var photoMessage = new ImageMessage();
+const emptyMessage = Message.newEmptyMessage();
+const textMessage = new Message('Yesterday message', Date.now() - 86400);
+const photoMessage = new ImageMessage();
 
 // Printing objects
 console.log(emptyMessage);
@@ -64,11 +66,11 @@ console.log(String(photoMessage));
 console.log(new Date(textMessage.created).toString());
 
 // Property shorthand
-var text = 'Some text';
-var created = Date.now();
-var duckTypeMessage = {
+const text = 'Some text';
+const created = Date.now();
+const duckTypeMessage = {
   text,
-  created
+  created,
 };
 console.log(duckTypeMessage);
 
